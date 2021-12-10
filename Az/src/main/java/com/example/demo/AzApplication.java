1b8047fd61105d271cc4cb7d0b293cfe7711bc98
Niveditha <niveditharc11@gmail.com>
2021-12-10 15:30:57 +0530

    Dec

---
+++ Az/src/main/java/com/example/demo/AzApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzApplication {
+
+	public static void main(String[] args) {
+		System.out.println("aa");
+		SpringApplication.run(AzApplication.class, args);
+	}
+
+}
+
+
+//eclipse web developer 3.23
